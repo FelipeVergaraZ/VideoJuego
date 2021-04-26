8e7c3da2108e945de16e9404c1f7beede35a9e49
Felipe Vergara <fel.vergaraz@duocuc.cl>
2021-04-25 20:46:46 -0400

    Formulario validado

--- Java/script.js
+++ Java/script.js
@@ -1,9 +1,38 @@
-
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function validarnumero() {
+    var x, text;
+
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("numb").value;
+
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "numero invalido";
+    } else {
+        text = "numero valido";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
+
+function validarusuario() {
+
+    var y = document.forms["myForm"]["fname"].value;
+    if (y == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+function validar() {
+    validarusuario();
+    validarnumero();
 }
